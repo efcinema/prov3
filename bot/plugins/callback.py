8e02c5d5dbf2607f0c481a9df6a8953ad5a3a677
efcinema <94225542+efcinema@users.noreply.github.com>
2021-11-28 11:28:32 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<b>🗂️Total File :- {leng}</b>\n<b>🎬Movie Name :-</b> <code>{query}</code>"
+    text=f"<b>🗂️Total File :- {leng}</b>\n<b>🎬Movie Name :-</b> <code>{query}/n ©️EFCINEMA</code>"
         
     try:
         await update.message.edit(
